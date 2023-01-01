de7f7f0d025d36d99f7a883813c2a2d18f5b3d1e
drapail <mv.dibrov@gmail.com>
2023-01-01 18:53:10 +0100

    refactored init_db to postgres syntax

--- src/main/resources/init_db.sql
+++ src/main/resources/init_db.sql
@@ -1,22 +1,26 @@
+DROP TABLE IF EXISTS cars_drivers;
+DROP TABLE IF EXISTS drivers;
+DROP TABLE IF EXISTS cars;
+DROP TABLE IF EXISTS manufacturers;
 -- ----------------------------
 -- Table structure for drivers
 -- ----------------------------
-DROP TABLE IF EXISTS drivers;
-CREATE TABLE drivers
+CREATE TABLE  drivers
 (
     id             SERIAL,
     name           varchar(255) NOT NULL,
     login          varchar(255) NOT NULL,
     password       varchar(255) NOT NULL,
     license_number varchar(255) NOT NULL,
-    is_deleted     bit(1)       NOT NULL DEFAULT 0,
+    is_deleted     bool         NOT NULL DEFAULT FALSE,
     PRIMARY KEY (id)
 );
-
+ALTER SEQUENCE drivers_id_seq AS bigint;
+ALTER TABLE drivers
+ALTER COLUMN id TYPE bigint USING id::bigint;
 -- ----------------------------
 -- Table structure for manufacturers
 -- ----------------------------
-DROP TABLE IF EXISTS manufacturers;
 CREATE TABLE manufacturers
 (
     id         SERIAL,
@@ -25,11 +29,12 @@ CREATE TABLE manufacturers
     is_deleted boolean      NOT NULL DEFAULT FALSE,
     PRIMARY KEY (id)
 );
-
+ALTER SEQUENCE manufacturers_id_seq AS bigint;
+ALTER TABLE manufacturers
+ALTER COLUMN id TYPE bigint USING id::bigint;
 -- ----------------------------
 -- Table structure for cars
 -- ----------------------------
-DROP TABLE IF EXISTS cars;
 CREATE TABLE cars
 (
     id              SERIAL,
@@ -39,11 +44,12 @@ CREATE TABLE cars
     PRIMARY KEY (id),
     CONSTRAINT FK_manufacturer_id FOREIGN KEY (manufacturer_id) REFERENCES manufacturers (id) ON DELETE RESTRICT ON UPDATE RESTRICT
 );
-
+ALTER SEQUENCE cars_id_seq AS bigint;
+ALTER TABLE cars
+ALTER COLUMN id TYPE bigint USING id::bigint;
 -- ----------------------------
 -- Table structure for cars_drivers
 -- ----------------------------
-DROP TABLE IF EXISTS cars_drivers;
 CREATE TABLE cars_drivers
 (
     car_id    bigint NOT NULL,
@@ -52,6 +58,3 @@ CREATE TABLE cars_drivers
     CONSTRAINT car_id FOREIGN KEY (car_id) REFERENCES cars (id) ON DELETE RESTRICT ON UPDATE RESTRICT,
     CONSTRAINT driver_id FOREIGN KEY (driver_id) REFERENCES drivers (id) ON DELETE RESTRICT ON UPDATE RESTRICT
 );
-
-SET
-FOREIGN_KEY_CHECKS = 1;
